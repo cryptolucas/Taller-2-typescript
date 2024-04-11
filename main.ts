d05e8c1a7e014d4f2439e15b94bf231e3c9d9467
Lucas Mejia <l.mejiat@uniandes.edu.co>
2024-04-11 00:14:46 -0500

    avance taller 2

--- main.ts
+++ main.ts
@@ -3,15 +3,36 @@ import { dataCourses } from './data.js';
 
 const seriesTbody: HTMLElement = document.getElementById("series")!; 
 
+const tarjetaContainer = document.getElementById("tarjetaContainer")!;
+
 renderCoursesInTable(dataCourses);
 
 function renderCoursesInTable(series: Serie[]): void {
   series.forEach(c => {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${c.id}</td>
-                           <td>${c.nombre}</td>
+                          <td><a href="#${c.nombre}">${c.nombre}</a></td>
                            <td>${c.canal}</td>
                            <td>${c.temporadas}</td>`;
     seriesTbody.appendChild(trElement);
+
+    trElement.addEventListener("click", () => mostrarTarjeta(c));
   });
+}
+
+
+
+function mostrarTarjeta(serie: Serie) {
+  const tarjetaHTML = `
+    <div class="card">
+      <div class="card-body">
+       <img src= "${serie.imagen}" style="height: 300px; width: 500px;>
+        <h5 class="card-title">${serie.nombre}</h5>
+        <p class="card-text"> ${serie.descripcion}</p>
+        <a href="${serie.enlace}"> ${serie.enlace} </a>
+      </div>
+    </div>
+  `;
+
+  tarjetaContainer.innerHTML = tarjetaHTML;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
 import { dataCourses } from './data.js';
 var seriesTbody = document.getElementById("series");
+var tarjetaContainer = document.getElementById("tarjetaContainer");
 renderCoursesInTable(dataCourses);
 function renderCoursesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                           <td>").concat(c.nombre, "</td>\n                           <td>").concat(c.canal, "</td>\n                           <td>").concat(c.temporadas, "</td>");
+        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                          <td><a href=\"#").concat(c.nombre, "\">").concat(c.nombre, "</a></td>\n                           <td>").concat(c.canal, "</td>\n                           <td>").concat(c.temporadas, "</td>");
         seriesTbody.appendChild(trElement);
+        trElement.addEventListener("click", function () { return mostrarTarjeta(c); });
     });
+}
+function mostrarTarjeta(serie) {
+    var tarjetaHTML = "\n    <div class=\"card\">\n      <div class=\"card-body\">\n       <img src= \"".concat(serie.imagen, "\" style=\"height: 300px; width: 500px;>\n        <h5 class=\"card-title\">").concat(serie.nombre, "</h5>\n        <p class=\"card-text\"> ").concat(serie.descripcion, "</p>\n        <a href=\"").concat(serie.enlace, "\"> ").concat(serie.enlace, " </a>\n      </div>\n    </div>\n  ");
+    tarjetaContainer.innerHTML = tarjetaHTML;
 }
